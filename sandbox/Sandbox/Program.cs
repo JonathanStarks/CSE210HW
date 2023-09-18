8c1f95c259a4d094bbe1cf7a715a7afea9149938
Jonathan Starks <jonstarks@byui.edu>
2023-09-18 09:54:15 -0600

    did the guessing number game assignment

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -5,5 +5,10 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Sandbox World!");
+
+        Console.Write("What's your name: ");
+        string user_name = Console.ReadLine();
+        Console.WriteLine("Hello, " + user_name + "!");
+        Console.WriteLine($"Hello, {user_name}!");
     }
 }
