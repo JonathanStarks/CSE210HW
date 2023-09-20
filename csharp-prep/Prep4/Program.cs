cf0fc8ceec8262f37cd06a679a5aebb76e620843
Jonathan Starks <jonstarks@byui.edu>
2023-09-20 17:06:30 -0600

    I couldnt take it anymore, I was so confused on how to do this.

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,17 +1,27 @@
+
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
+
         //Console.WriteLine("Hello Prep4 World!");
         // Asks for numbers untill the user enters a zero and adds them to the list
-
+        List<int> numbers = new List<int>();
         int user_input = -1;
 
         do {
-        Console.Write("enter numbers in whole intigers only: ");
-        
+        Console.Write("Please enter numbers in whole intigers only: ");
+        user_input = int.Parse(Console.ReadLine());
+        numbers.Add(user_input);
         } while (user_input != 0);
+        
+        int total = 0;
+        foreach (int number in numbers){
+            total += number;
+        }
+        Console.WriteLine(total);
     }
 }
