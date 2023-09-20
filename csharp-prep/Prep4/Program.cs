cbb475374f5064f2eb9286b8484894d2c28d5aff
Jonathan Starks <jonstarks@byui.edu>
2023-09-20 09:25:55 -0600

    Trying out the new branch.

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        //Console.WriteLine("Hello Prep4 World!");
+        // Asks for numbers untill the user enters a zero and adds them to the list
+
+        int user_input = -1;
+
+        do {
+        Console.Write("enter numbers in whole intigers only: ");
+        
+        } while (user_input != 0);
     }
 }
